0daf860101b984a3cbd039e5ed08eac7343d522f
kicekream <kicekream@gmail.com>
2021-11-04 01:16:46 +0100

    modified db config

--- src/database/modelIndex.js
+++ src/database/modelIndex.js
@@ -1,8 +1,10 @@
 const { Pool } = require("pg");
 require("dotenv/config");
 
-
-const string = process.env.DATABASE_URL;
+const string =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
 
 let connect = { connectionString: string, 
     ssl: { rejectUnauthorized: false } };
--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,8 +1,12 @@
 const users = require ("./user");
+const transaction = require("./transaction")
+const wallet = require("./wallet")
 
 module.exports = async (client) => {
     try {
         await client.query(users.CREATE_TABLE);
+        await client.query(transaction.CREATE_TABLE);
+        await client.query(wallet.CREATE_TABLE);
         console.log(`Table(s) Created`);
     }
     catch(error) {
